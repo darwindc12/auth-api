c9e291a02015386812cb22b34558681be354d4ec
datzmi <darwindelacruz0012@gmail.com>
2026-03-16 21:47:28 +0800

    Update serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,33 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+
+class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = [
+            'username',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'role'
+        ]
 
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
+
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            role=validated_data.get('role', 'user')
         )
+
         return user
